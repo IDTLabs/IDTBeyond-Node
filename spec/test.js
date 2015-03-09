1f489fab6df8461685535c514accc8d66bafa77d
Robbie Holmes <robbiethegeek@gmail.com>
2015-03-09 15:37:16 -0400

    Have a working test suite now, and filling out the tests, that will drive more features.

--- spec/test.js
+++ spec/test.js
@@ -1,14 +1,27 @@
-var idtBeyond = require('../lib/idt-beyond');
-console.log(JSON.stringify(idtBeyond, null, 2))
-//
-//describe('JavaScript addition operator', function () {
-//  it('adds two numbers together', function () {
-//    expect(1 + 2).toEqual(3);
-//  });
-//});
+var idtBeyondApi = require('../lib/idt-beyond');
 
 describe('Create idtBeyond object', function() {
-  it("should error if you don't pass both parameters", function() {
-    expect(idtBeyond.initialize()).toThrow('Both Application ID (appId) and Application Key (appKey) are required.');
+  describe('should error', function(){
+    it("if you don't pass an options object", function() {
+      expect(function(){
+        idtBeyondApi.initialize()
+      }).toThrow();
+    });
+    it("if there in no appId in the options object", function(){
+      expect(function(){
+        idtBeyondApi.initialize({appKey: 'app-key'})
+      }).toThrow();
+    });
+  });
+  describe('should be successful', function(){
+    it("if you pass an options object with both an appId and appKey", function() {
+      console.log(idtBeyondApi.initialize({appId: 'app-id', appKey: 'app-key'}))
+      var idtBeyond = idtBeyondApi.initialize({appId: 'app-id', appKey: 'app-key'});
+      expect(idtBeyond).toEqual(jasmine.any(Object));
+      expect(idtBeyond.hasOwnProperty('getAppId')).toBe(true);
+      expect(idtBeyond.hasOwnProperty('getAppKey')).toBe(true);
+      expect(idtBeyond.getAppId()).toBe('app-id');
+      expect(idtBeyond.getAppKey()).toBe('app-key');
+    });
   });
 });
