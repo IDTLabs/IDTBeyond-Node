e0de2cfe159df8ae1c59a6cc9eac5c4f2c389c40
Robbie Holmes <robbiethegeek@gmail.com>
2015-03-09 18:31:02 -0400

    All functions added to the library, need to round out the tests."

--- spec/test.js
+++ spec/test.js
@@ -1,27 +1,60 @@
 var idtBeyondApi = require('../lib/idt-beyond');
+require("jasmine-as-promised")();
 
-describe('Create idtBeyond object', function() {
-  describe('should error', function(){
-    it("if you don't pass an options object", function() {
-      expect(function(){
-        idtBeyondApi.initialize()
-      }).toThrow();
+describe('IDT Beyond API', function() {
+  describe('instantiating the object', function(){
+
+    describe('should error', function(){
+      it("if you don't pass an options object", function() {
+        expect(function(){
+          idtBeyondApi.initialize()
+        }).toThrow();
+      });
+
+      it("if there in no appId in the options object", function(){
+        expect(function(){
+          idtBeyondApi.initialize({appKey: 'app-key'})
+        }).toThrow();
+      });
     });
-    it("if there in no appId in the options object", function(){
-      expect(function(){
-        idtBeyondApi.initialize({appKey: 'app-key'})
-      }).toThrow();
+
+    describe('should be successful', function(){
+      it("if you pass an options object with both an appId and appKey", function() {
+        var idtBeyond = idtBeyondApi.initialize({appId: 'app-id', appKey: 'app-key'});
+        expect(idtBeyond).toEqual(jasmine.any(Object));
+        expect(idtBeyond.hasOwnProperty('getAppId')).toBe(true);
+        expect(idtBeyond.hasOwnProperty('getAppKey')).toBe(true);
+        expect(idtBeyond.getAppId()).toBe('app-id');
+        expect(idtBeyond.getAppKey()).toBe('app-key');
+      });
     });
   });
-  describe('should be successful', function(){
-    it("if you pass an options object with both an appId and appKey", function() {
-      console.log(idtBeyondApi.initialize({appId: 'app-id', appKey: 'app-key'}))
-      var idtBeyond = idtBeyondApi.initialize({appId: 'app-id', appKey: 'app-key'});
-      expect(idtBeyond).toEqual(jasmine.any(Object));
-      expect(idtBeyond.hasOwnProperty('getAppId')).toBe(true);
-      expect(idtBeyond.hasOwnProperty('getAppKey')).toBe(true);
-      expect(idtBeyond.getAppId()).toBe('app-id');
-      expect(idtBeyond.getAppKey()).toBe('app-key');
+  describe('Once the object is instantiated', function(){
+    describe('should be able to interact with the API', function(){
+      describe('should error', function(){
+        it("if you call any API endpoints with an invalid appKey/appId", function(done) {
+          var idtBeyond = idtBeyondApi.initialize({appId: 'app-id', appKey: 'app-key'});
+          runs(
+              function() {
+                return idtBeyond.getProducts();
+              },
+              function checksExpections(result) {
+                expect(result.toEqual({success: false, message: 'asdfs'}));
+              });
+
+
+
+          //done();
+          //console.log(products)
+          //
+          //expect(function(){
+          //      //idtBeyond.getProducts();
+          //  done();
+          //}).toThrow();
+        });
+      });
     });
+
   });
+
 });
